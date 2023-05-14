7ee69a3a7991050f268154408ed2272f9ae14601
Krzysiek <107618391+KrzysztofBuda@users.noreply.github.com>
2023-05-14 10:57:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
-#include "mainwindow.h"
+//main.cpp
 
+#include "mainwindow.h"
 #include <QApplication>
+#include <fstream>
+#include <cstdlib>
+#include <QDebug>
+#include <iostream>
+#include "Pizza.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     return a.exec();
 }
